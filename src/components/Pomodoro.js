8e9ca1c15288add274da2b3329657ee140966fe1
KairstenFay <kairsten.fay@gmail.com>
2019-03-14 15:19:13 -0700

    Start/stop button now works on clock

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -11,20 +11,41 @@ export default class Pomodoro extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            time: 1500
+            time: 1500,
+            playing: false,
+            timeout: null
         };
         this.countdown = this.countdown.bind(this);
         this.start = this.start.bind(this);
+        this.togglePlay = this.togglePlay.bind(this);
     }
 
     countdown() {
         this.setState({
             time: this.state.time - 1
         });
+        this.start();  // what is this witchcraft??
     }
+
+    togglePlay() {
+        console.log("register toggle");
+        if (!this.state.playing) {
+            this.start();
+        } else {
+            clearTimeout(this.state.timeout);
+        }
+
+        this.setState({
+            playing: !this.state.playing
+        })
+    }
+
     start() {
-        setTimeout(this.countdown(), 1000);
+        this.setState({
+            timeout: setTimeout(this.countdown, 1000)
+        })
     }
+
     render() {
         return(
             <div id="pomodoro">
@@ -32,7 +53,7 @@ export default class Pomodoro extends Component {
                 <Session />
                 <Clock time={this.state.time} />
                 <img id="start_stop" className="control" src={play_pause} alt="play/pause button"
-                    onClick={this.start}/>
+                    onClick={this.togglePlay}/>
                 <img id="reset" className="control" src={reset} alt="reset button" />
             </div>
         )
