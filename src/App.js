3c07236945ef92f468931352134aa537d726f82c
KairstenFay <kairsten.fay@gmail.com>
2019-03-16 11:12:06 -0700

    Added google analytics via react-ga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Pomodoro from './components/Pomodoro';
 import './App.css';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-127100738-6');
+
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 class App extends Component {
   render() {
